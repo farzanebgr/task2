557e042384ae0bc7be3bac2e55a10fb4db3e67aa
farzanebgr <farzenehboogari@gmail.com>
2022-08-28 11:56:20 +0430

    Use a router for a better URL

--- homeApp/api/urls.py
+++ homeApp/api/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 from homeApp.api import views
 
+router = DefaultRouter()
+router.register('slider', views.IndexSliderDetailsAV, basename='slider')
+router.register('latest-products', views.LatestProductsDetailsAV, basename='latest-products')
+router.register('most-visit-products', views.MostVisitProductsDetailsAV, basename='most-visit-products')
+router.register('about-us', views.AboutUsDetailsAV, basename='about-us')
+
 urlpatterns = [
-    path('slider/', views.IndexSliderAV.as_view(), name='slider'),
-    path('slider/<int:pk>/', views.IndexSliderDetailsAV.as_view(), name='slider-details'),
-    path('latest-products/', views.LatestProductsAv.as_view(), name='latest-products'),
-    path('latest-products/<int:pk>/', views.LatestProductsDetailsAV.as_view(), name='latest-products-details'),
-    path('most-visit-products/', views.MostVisitProductsAV.as_view(), name='most-visit-products'),
-    path('most-visit-products/<int:pk>/', views.MostVisitProductsDetailsAV.as_view(), name='most-visit-products-details'),
-    path('about-us/', views.AboutUsAV.as_view(), name='about-us'),
-    path('about-us/<int:pk>/', views.AboutUsDetailsAV.as_view(), name='about-us-details'),
+    path('', include(router.urls)),
+    path('', include(router.urls)),
+    path('', include(router.urls)),
+    path('', include(router.urls)),
 ]
--- homeApp/api/views.py
+++ homeApp/api/views.py
@@ -1,60 +1,33 @@
 from rest_framework import generics
-
+from rest_framework import viewsets
 from homeApp.api.serializers import SliderSerializer, ProductsSerializer, AboutUsSerializer
 from django.db.models import Count
 from django.shortcuts import render
-from django.views.generic import TemplateView
 from siteSettingsApp.models import settingModel, footerLinkBox, Slider
 from productionsApp.models import Products
-from utils.convertors import group_list
 
 
-# Show all active sliders and create new one
-class IndexSliderAV(generics.ListCreateAPIView):
-    queryset = Slider.objects.filter(isActive=True)
-    serializer_class = SliderSerializer
-
-
-# Show a particular active sliders and Retrieve Update Destroy APIView
-class IndexSliderDetailsAV(generics.RetrieveUpdateDestroyAPIView):
+# Show all sliders and Retrieve Update Destroy APIView a particular slider
+class IndexSliderDetailsAV(viewsets.ModelViewSet):
     queryset = Slider.objects.all()
     serializer_class = SliderSerializer
 
 
-# Show 12 active products and create new one
-class LatestProductsAv(generics.ListCreateAPIView):
-    queryset = Products.objects.filter(isActive=True, isDelete=False)[:12]
-    serializer_class = ProductsSerializer
-
-
 # Show a particular active product and Retrieve Update Destroy APIView
-class LatestProductsDetailsAV(generics.RetrieveUpdateDestroyAPIView):
+class LatestProductsDetailsAV(viewsets.ModelViewSet):
     queryset = Products.objects.filter(isActive=True, isDelete=False).all()
     serializer_class = ProductsSerializer
 
 
-# Show 12 most visit products and create new one
-class MostVisitProductsAV(generics.ListCreateAPIView):
-    queryset = Products.objects.filter(isActive=True, isDelete=False).annotate(
-        visit_count=Count('productsvisit')).order_by('-productsvisit')[:12]
-    serializer_class = ProductsSerializer
-
-
 # Show a particular most visit product and Retrieve Update Destroy APIView
-class MostVisitProductsDetailsAV(generics.RetrieveUpdateDestroyAPIView):
+class MostVisitProductsDetailsAV(viewsets.ModelViewSet):
     queryset = Products.objects.filter(isActive=True, isDelete=False).annotate(
         visit_count=Count('productsvisit')).all()
     serializer_class = ProductsSerializer
 
 
-# Show active site setting model and create new one
-class AboutUsAV(generics.ListCreateAPIView):
-    queryset = settingModel.objects.filter(isMainSettings=True).all()
-    serializer_class = AboutUsSerializer
-
-
 # Show particular active site setting model and Retrieve Update Destroy APIView
-class AboutUsDetailsAV(generics.RetrieveUpdateDestroyAPIView):
+class AboutUsDetailsAV(viewsets.ModelViewSet):
     queryset = settingModel.objects.filter(isMainSettings=True).all()
     serializer_class = AboutUsSerializer
 
