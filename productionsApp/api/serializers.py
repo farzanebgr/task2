7322301c008503f6271d56e0a53f8e3262547806
farzanebgr <farzenehboogari@gmail.com>
2022-09-03 13:45:20 +0430

    Filter products

--- productionsApp/api/serializers.py
+++ productionsApp/api/serializers.py
@@ -46,7 +46,6 @@ class ProductsTagsSerializer(serializers.ModelSerializer):
 
 # Serializer for Products
 class ProductsSerializer(serializers.ModelSerializer):
-    productsvisit = serializers.StringRelatedField(read_only=True, many=True)
     category = serializers.StringRelatedField(read_only=True, many=True)
     brand = serializers.StringRelatedField(read_only=True)
 
