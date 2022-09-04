5626a256b04f32df77f7f63070d3889699961370
farzanebgr <farzenehboogari@gmail.com>
2022-09-04 17:51:20 +0430

    settings for all apps

--- userAccountApp/models.py
+++ userAccountApp/models.py
@@ -20,9 +20,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.get_full_name()
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
