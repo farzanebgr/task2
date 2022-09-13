cdaa47853dce6c4849ef00ce7da5468c03c5e630
farzanebgr <farzenehboogari@gmail.com>
2022-09-13 10:45:14 +0430

    Add the Django Channels library

--- shopCenter/settings.py
+++ shopCenter/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
-    # 'rest_framework.authtoken'
+    'channels',
 ]
 
 SIMPLE_JWT = {
